fe87c32346f796ad9c966bb3bcfe4881067423ae
Matheus Angellos <matheus.angellos@townsq.com.br>
2018-06-19 15:55:33 -0300

    Update. export ANDROID_HOME=/Users/mazoni/android-sdk

---
+++ angular2/01-cadastro-contatos/app/contatos/contato.model.ts
@@ -0,0 +1,13 @@
+export class Contato {
+
+    constructor(
+        public id: number,
+        public nome: string,
+        public email: string,
+        public telefone: string,
+    ){}
+}
+
+
+let c: Contato = new Contato(1, 'Fulano', 'email', 'telefone');
+
---
+++ angular2/01-cadastro-contatos/app/contatos/contatos-mock.ts
@@ -0,0 +1,8 @@
+import { Contato } from './contato.model';
+
+export const CONTATOS: Contato[] = [
+    {id: 1, nome: 'Fulano de Tal', email: 'fulano@gmail.com', telefone: '(00) 0000-0000'},
+    {id: 2, nome: 'Ciclano', email: 'ciclano@gmail.com', telefone: '(00) 0000-0000'},
+    {id: 3, nome: 'Seu Madruga', email: 'madruga@gmail.com', telefone: '(00) 0000-0000'},
+    {id: 4, nome: 'Bob Esponja', email: 'bob@gmail.com', telefone: '(00) 0000-0000'},
+];
